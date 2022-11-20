2b7ef5074e474100fa8c573d0921fc5d9710ebd5
Ext-M.Savich <Msavich@it-one.ru>
2022-11-20 23:06:56 +0600

    Пятый комит.
    Поправил класс Lion (в классе была грамматическая ошибка)
    Написал тесты для Lion
    плагин jacoco  показывает покрытие всего кода на 92%

--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -1,2 +1,79 @@
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+
+@RunWith(Parameterized.class)
 public class LionTest {
+
+    @Mock
+    Feline feline;
+
+    @Parameterized.Parameters
+    public static Object[][] getData() {
+        return new Object[][]{
+                {"Самец", true},
+                {"Самка", false},
+                {"Другое", false},
+        };
+    }
+
+    @Before
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    String sex;
+    boolean hasMane;
+
+    public LionTest(String sex, boolean hasMane) {
+        this.sex = sex;
+        this.hasMane = hasMane;
+    }
+
+    @Test
+    public void getKittensCountCheck() {
+        try {
+            Lion lion = new Lion(sex, feline);
+            Mockito.when(lion.getKittens()).thenReturn(1);
+            Assert.assertEquals(1, lion.getKittens());
+        } catch (Exception e) {
+            errorMessageCheck(e);
+        }
+    }
+
+    @Test
+    public void doesHaveManeCheck() {
+        try {
+            Lion lion = new Lion(sex, feline);
+            Assert.assertEquals(hasMane, lion.doesHaveMane());
+        } catch (Exception e) {
+            errorMessageCheck(e);
+        }
+    }
+
+    @Test
+    public void getFoodCheck() {
+        try {
+            Lion lion = new Lion(sex, feline);
+            List<String> expectedList = List.of("Животные", "Птицы", "Рыба");
+            Mockito.when(lion.getFood()).thenReturn(expectedList);
+            Assert.assertEquals(expectedList, lion.getFood());
+        } catch (Exception e) {
+            errorMessageCheck(e);
+        }
+    }
+
+    void errorMessageCheck(Exception e) {
+        String message = "Используйте допустимые значения пола животного - самец или самка";
+        Assert.assertEquals(message, e.getMessage());
+    }
 }
